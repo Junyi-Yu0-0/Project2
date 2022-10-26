ac97da82312f209f2ace27fb7b7478540f1a330a
Junyi-Yu0-0 <61995892+Junyi-Yu0-0@users.noreply.github.com>
2022-10-26 12:13:46 -0700

    anime.js

---
+++ anime.js
@@ -0,0 +1,9 @@
+let myAimation = anime({
+        targets: '.square',
+  translateX: 100,
+  borderRadius: 50,
+  duration: 2000,
+  easing: 'linear',
+  loop: true,
+  direction: 'alternate',
+      });
