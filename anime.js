0ab513e37a61619021996e994b9fe5d0d51dc99d
Junyi-Yu0-0 <61995892+Junyi-Yu0-0@users.noreply.github.com>
2022-10-31 20:46:37 -0700

    anime

--- anime.js
+++ anime.js
@@ -1,9 +1,41 @@
-let myAimation = anime({
-        targets: '.square',
-  translateX: 100,
-  borderRadius: 50,
-  duration: 2000,
-  easing: 'linear',
-  loop: true,
-  direction: 'alternate',
-      });
+// let myAimation = anime({
+//         targets: '.square',
+//   translateX: 100,
+//   borderRadius: 50,
+//   duration: 2000,
+//   easing: 'linear',
+//   loop: true,
+//   direction: 'alternate',
+//       });
+
+  anime({
+  targets: '.bat',
+  translateX: {
+    value: 80,
+    duration: 80
+  },
+  loop:true,
+  rotate: {
+    value: 200,
+    duration: 1800,
+    easing: 'easeInOutSine'
+  },
+  loop:true,
+  delay: 250 // All properties except 'scale' inherit 250ms delay
+});
+
+anime({
+targets: '.bat1',
+translateX: {
+  value: -80,
+  duration: -80
+},
+loop:true,
+rotate: {
+  value: -200,
+  duration: 1800,
+  easing: 'easeInOutSine'
+},
+loop:true,
+delay: 250 // All properties except 'scale' inherit 250ms delay
+});
